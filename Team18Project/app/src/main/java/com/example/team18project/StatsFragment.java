89fd5199cfd96e018bd47b72b82097bdd51f8de0
Rayan Alouzi <rayanbassam3@gmail.com>
2023-03-03 23:18:12 -0700

    successful merge between Rayan and Dom's branch. finished high and low score fragment

--- Team18Project/app/src/main/java/com/example/team18project/StatsFragment.java
+++ Team18Project/app/src/main/java/com/example/team18project/StatsFragment.java
@@ -19,9 +19,11 @@ public class StatsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private static final String ARG_PARAM3 = "param3";
     // TODO: Rename and change types of parameters
-    private int mParam1;
+    private int totalScore;
+    private int high_score;
+    private int low_score;
 
     /**
      * Use this factory method to create a new instance of
@@ -31,10 +33,12 @@ public class StatsFragment extends Fragment {
      * @return A new instance of fragment StatsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StatsFragment newInstance(int totalScore) {
+    public static StatsFragment newInstance(int totalScore,int highest_score,int lowest_score) {
         StatsFragment fragment = new StatsFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_PARAM1, totalScore);
+        args.putInt(ARG_PARAM2, highest_score);
+        args.putInt(ARG_PARAM3, lowest_score);
         fragment.setArguments(args);
         return fragment;
     }
@@ -43,7 +47,9 @@ public class StatsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getInt(ARG_PARAM1);
+            totalScore = getArguments().getInt(ARG_PARAM1);
+            high_score = getArguments().getInt(ARG_PARAM2);
+            low_score = getArguments().getInt(ARG_PARAM3);
         }
 
     }
@@ -53,8 +59,13 @@ public class StatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_stats, null);
         TextView tScore = view.findViewById(R.id.TotalScore);
-        tScore.setText(Integer.toString(mParam1));
+        tScore.setText(Integer.toString(totalScore));
 
+        TextView high_score = view.findViewById(R.id.highest_score);
+        high_score.setText(Integer.toString(this.high_score));
+
+        TextView low_score = view.findViewById(R.id.lowest_score);
+        low_score.setText(Integer.toString(this.low_score));
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_stats, container, false);
         return view;
