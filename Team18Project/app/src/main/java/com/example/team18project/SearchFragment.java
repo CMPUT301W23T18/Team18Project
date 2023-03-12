89f1576564c2b3d703b43f8dfe86bdf1e8c8903c
Rayan Alouzi <rayanbassam3@gmail.com>
2023-03-11 19:32:15 -0700

    searching username possible

--- Team18Project/app/src/main/java/com/example/team18project/SearchFragment.java
+++ Team18Project/app/src/main/java/com/example/team18project/SearchFragment.java
@@ -1,11 +1,27 @@
 package com.example.team18project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -13,34 +29,21 @@ import androidx.fragment.app.Fragment;
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
+    public interface UserListCallback {
+        void onUserListGenerated(ArrayList<Pair<String, String>> userList);
     }
+    private FirebaseFirestore db;
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment SearchFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
+    public static SearchFragment newInstance() {
         SearchFragment fragment = new SearchFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,15 +52,59 @@ public class SearchFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        EditText usernameSearchEditText = view.findViewById(R.id.username_search);
+        Button searchButton = view.findViewById(R.id.search_button);
+        ListView userListView = view.findViewById(R.id.user_list);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("worked", "worked");
+                String searchText = usernameSearchEditText.getText().toString();
+                generateUserList(new UserListCallback() {
+                    @Override
+                    public void onUserListGenerated(ArrayList<Pair<String, String>> userList) {
+                        ArrayList<Pair<String, String>> filteredUserList = new ArrayList<>();
+                        for (Pair<String, String> item : userList) {
+                            if (item.second.toLowerCase().contains(searchText.toLowerCase())) {
+                                filteredUserList.add(item);
+                            }
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, filteredUserList);
+                        userListView.setAdapter(adapter);
+                    }
+                });
+            }
+        });
+
+        return view;
+    }
+
+    public void generateUserList(final UserListCallback callback) {
+        db = FirebaseFirestore.getInstance();
+        db.collection("Players").whereEqualTo("isHidden", false)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        ArrayList<Pair<String, String>> userList = new ArrayList<>();
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Pair<String, String> newUser = new Pair<>(document.getId(), document.get("username").toString());
+                                userList.add(newUser);
+                            }
+                            callback.onUserListGenerated(userList);
+                        } else {
+                            Log.d("SearchError:", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
