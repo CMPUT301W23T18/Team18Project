0f0d5d1ed9df5e8bcc52ea355434ab546485b069
dreambigzhang <alan.yushu.zhang@gmail.com>
2023-03-12 23:58:34 -0600

    AllQRCodeFragment frontend now working

--- Team18Project/app/src/main/java/com/example/team18project/AllQRCodesFragment.java
+++ Team18Project/app/src/main/java/com/example/team18project/AllQRCodesFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -44,12 +46,20 @@ public class AllQRCodesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
         otherQRCodeList = new ArrayList<QRCode>();
         qrListView = (ListView) getView().findViewById(R.id.other_qrcode_list);
         qrAdapter = new QRArrayAdapter(getContext(), otherQRCodeList);
         qrListView.setAdapter(qrAdapter);
         getAllQRCode(otherQRCodeList, qrAdapter);
     }
+
 
     private void getAllQRCode(ArrayList<QRCode> otherQRCodeList, QRArrayAdapter qrArrayAdapter) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
