646680745b38e3f57b4b7a60772744b25565be6b
Dominyk Gallatin <gallatin@ualberta.ca>
2023-04-02 22:57:31 -0700

    dom

--- Team18Project/app/src/main/java/com/example/team18project/view/ScanQRCode.java
+++ Team18Project/app/src/main/java/com/example/team18project/view/ScanQRCode.java
@@ -196,7 +196,7 @@ public class ScanQRCode extends AppCompatActivity {
                         QRCode code = new QRCode(finalSha25, new ArrayList<String>(), new ArrayList<Comment>(), latitude, longitude);
                         code.setQid(id);
                         player.addQRCode(code);
-
+                        // please work
                     }
                 });
 
